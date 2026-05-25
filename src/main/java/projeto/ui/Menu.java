a1cecdb0241a46b504bc1712ac99ddaffd012440
cristianacosta17cc <cristianacosta17.cc@gmail.com>
2026-05-25 19:44:59 +0100

    Update Menu.java

--- src/main/java/projeto/ui/Menu.java
+++ src/main/java/projeto/ui/Menu.java
@@ -64,7 +64,7 @@ public class Menu {
 
         service.adicionarProprietario(proprietario);
 
-        System.out.println("Proprietário registado com sucesso.");
+        System.out.println("Proprietário registado .");
 
         // pergunta se deseja registar animal
         System.out.print("Deseja registar um animal agora? (s/n): ");
